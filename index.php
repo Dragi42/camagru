dec07a1972b75ee9e607fc51641181f88efab107
Dragan PAUNOVIC <dpaunovi@e2r2p11.42.fr>
2017-12-29 19:17:40 +0100

    css ok

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ session_start();
 		<link rel="stylesheet" type="text/css" href="css/index.css">
 	</head>
 	<body>
+	<div class="box">
 		<div class="container">
 			<h2>All of Pictures</h2>
 			<iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FLADbible%2Fvideos%2F4308500969197060%2F&show_text=0&width=476" style="max-width: 50%; max-height: 50%; min-width: 300px; min-height: 300px;" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>
@@ -23,6 +24,7 @@ session_start();
 ?>
 			</div>
 		</div>
+</div>
 		<div id="footer">
 			<hr />
 			<p>@2017 dpaunovi</p>
